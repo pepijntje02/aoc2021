1637af638c976f53c3329bcb493678448fce5e02
Pepijn van Tol <pepijn.van.tol@outlook.com>
2021-12-01 13:43:01 +0100

    add time

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,11 @@
+from time import perf_counter_ns
+import os, sys
+parentdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parentdir)
+
+from myDecorator import time_function
+
+
 with open('./input.txt') as f:
     data = [float(line) for line in f.readlines()]
 
@@ -5,24 +13,43 @@ def increase(data):
     f = lambda x, y: y > x
     return sum( list(map(f, data[:-1], data[1:])) )
 
-puzzle1 = increase(data)
-print(puzzle1)
+@time_function
+def puzzle1(data):
+    return increase(data)
+
+print(f"Puzzle 1: {puzzle1(data)}")
 
 def sliding_window(array):
     return [sum(array[i:i+3]) for i in range(len(array)-2)]
 
-three_sum = sliding_window(data)
 
-puzzle2 = increase(three_sum) 
-print(puzzle2)
+@time_function
+def puzzle2(data):
+    three_sum = sliding_window(data)
+    return increase(three_sum) 
+
+print(f"Puzzle 2: {puzzle2(data)}")
 
 # And now with thinking, i.e. use math instead of brute force
-# If the list is: 0, 1, 2, 3, 4 then the moving difference is:
-# (1 + 2 + 3) - (0 + 1 + 2) = 3-0 i.e. index 0 and index 2.
+# If the list is: a, b, c, d then the moving difference is:
+# (b + c + d) - (a + b + c) = d - a i.e. index 0 and index 2.
 # Rewrite increase function:
 
 def increasev2(data, step):
     f = lambda x, y: y > x
     return sum( list(map(f, data[:-step], data[step:])) )
 
-print(increasev2(data, step=3))
+@time_function
+def puzzle2v2(data):
+    return increasev2(data, step=3)
+
+print(f"Puzzle 2: {puzzle2v2(data)}")
+
+'''
+>>> Function puzzle1(*args, **kwargs): time elapsed: 0.218 [ms]
+>>> Puzzle 1: 1553
+>>> Function puzzle2(*args, **kwargs): time elapsed: 0.657 [ms]
+>>> Puzzle 2: 1597
+>>> Function puzzle2v2(*args, **kwargs): time elapsed: 0.199 [ms]
+>>> Puzzle 2: 1597
+'''
