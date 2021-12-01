d35367760ac2a4caad2da19595158b16fd13db16
Pepijn van Tol <pepijn.van.tol@outlook.com>
2021-12-01 09:33:02 +0100

    day1 improvement

--- day1/day1.py
+++ day1/day1.py
@@ -15,3 +15,14 @@ three_sum = sliding_window(data)
 
 puzzle2 = increase(three_sum) 
 print(puzzle2)
+
+# And now with thinking, i.e. use math instead of brute force
+# If the list is: 0, 1, 2, 3, 4 then the moving difference is:
+# (1 + 2 + 3) - (0 + 1 + 2) = 3-0 i.e. index 0 and index 2.
+# Rewrite increase function:
+
+def increasev2(data, step):
+    f = lambda x, y: y > x
+    return sum( list(map(f, data[:-step], data[step:])) )
+
+print(increasev2(data, step=3))
